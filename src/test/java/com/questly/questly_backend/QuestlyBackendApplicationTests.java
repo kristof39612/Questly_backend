33e0234a83c8956df98e469c46790c6e2b1ae344
kokairoli <kokairoli31@gmail.com>
2024-10-09 17:41:05 +0200

    init: project initialization

---
+++ src/test/java/com/questly/questly_backend/QuestlyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.questly.questly_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestlyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
