d675c359481d178dc67069601b94eb85df99117d
ta1120 <toth.adam1120@gmail.com>
2024-10-31 21:02:47 +0100

    Changed user Id type to Long

--- src/main/java/com/questly/questly_backend/model/User/User.java
+++ src/main/java/com/questly/questly_backend/model/User/User.java
@@ -22,7 +22,7 @@ public class User implements UserDetails {
 
     @Id
     @GeneratedValue
-    private Integer id;
+    private Long id;
 
     @Column(nullable = false)
     private String email;
